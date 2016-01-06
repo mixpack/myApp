97356e8d521745b7b22783e2768a99d8eba87eb2
Kathy Benfield <github@mixpack.nl>
2016-01-07 00:27:19 +0100

    Add famous friends

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,29 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Hwang Yoon Seok',
+    lastText: 'I\'m on my way?',
+    face: 'img/hwanhee.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Brian Joo',
+    lastText: 'Hey, it\'s me.',
+    face: 'img/brian.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'So Ji Sub',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/jisub.png'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Joo Sang Wook',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/sangwook.jpg'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Kim Soo Hyun',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/soohyun.jpg'
   }];
 
   return {
