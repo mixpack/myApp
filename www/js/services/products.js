10491ab66aab15a7ba7284c43699ce3a1210e161
Kathy Marsman <github@mixpack.nl>
2016-01-10 15:33:36 +0100

    Refactor services and controllers

---
+++ www/js/services/products.js
@@ -0,0 +1,29 @@
+'use strict';
+
+app.factory('Products', function(FURL, $firebaseArray) {
+  var ref = new Firebase(FURL);
+  var products = $firebaseArray(ref.child('products'));
+
+  var Products = {
+    all: function(){
+      return products;
+    },
+
+
+    saveProduct: function(product, image){
+      var newProduct = {
+        name: product.name,
+        tagline: product.tagline,
+        description: product.description,
+        price: product.price,
+        image: image
+      };
+      return products.$add(newProduct).then(function(){
+        console.log('new product added to the database');
+      })
+    }
+
+  };
+  return Products;
+
+});
